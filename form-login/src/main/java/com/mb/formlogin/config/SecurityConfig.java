340f96a58e9f8652bfed94cdeb4da6db333c022b
Mahesh Bhosale <233565011+bhosale-mahesh@users.noreply.github.com>
2026-07-12 19:56:11 +0530

    Add public and private endpoints with custom user details login

--- form-login/src/main/java/com/mb/formlogin/config/SecurityConfig.java
+++ form-login/src/main/java/com/mb/formlogin/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,9 +22,19 @@ public class SecurityConfig {
                 .httpBasic(withDefaults()) // Authorization: Basic ... header is used, in curl or API
                 .formLogin(withDefaults()) // Use form in browser
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers("/public").permitAll()
+                        .anyRequest().authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("mahesh")
+                        .password("{noop}pass")
+                        .build()
+        );
     }
 }
---
+++ form-login/src/main/java/com/mb/formlogin/contoller/FormController.java
@@ -0,0 +1,22 @@
+package com.mb.formlogin.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello!!! 😁";
+    }
+
+    @GetMapping("/public")
+    public String publicPage() {
+        return "Welcome to public page!!! 🥱";
+    }
+    @GetMapping("/private")
+    public String privatePage() {
+        return "Welcome to private page!!! 🤫";
+    }
+}
