fcd924d287f13a488776855d79e08483226b90a4
Mahesh Bhosale <233565011+bhosale-mahesh@users.noreply.github.com>
2026-08-08 16:10:30 +0530

    Update security config for form login

--- form-login/src/main/java/com/mb/formlogin/config/SecurityConfig.java
+++ form-login/src/main/java/com/mb/formlogin/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mb.formlogin.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,31 +11,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .httpBasic(withDefaults()) // Authorization: Basic ... header is used, in curl or API
-                .formLogin(withDefaults()) // Use form in browser
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public").permitAll()
-                        .anyRequest().authenticated()
-                );
+                .formLogin(withDefaults()); // Use form in browser
+
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("mahesh")
-                        .password("{noop}pass")
-                        .build()
-        );
+        return new InMemoryUserDetailsManager(User.withUsername("mahesh")
+                .password("{bcrypt}$2a$12$0L5igF4Hvqj4YX3sWoAfiO2hAzkakCzb63hVlwDMr4LXACp4G4FZO")
+                .build());
     }
 }
